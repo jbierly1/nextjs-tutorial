9b85aea430e241e83dad2c141b21e99927dcaeb4
James Bierly <james.bierly@gmail.com>
2024-12-29 17:22:11 -0500

    made first tutorial vid files

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+export const ProductCard = () => {
+  return (
+    <div className="" >
+     <AddToCart />
+
+    </div>
+  )
+}
+
+export default ProductCard;
